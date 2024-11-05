231c4ced1eb250532bdce6447684c46e403d4e7f
D1GQ <yourboylax@gmail.com>
2024-11-05 16:32:12 -0600

    new commands for later

---
+++ Commands/BaseCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace TheBetterRoles.Commands;
+
+public enum ArgumentType
+{
+    None,
+    Player,
+}
+
+public abstract class TBRCommandArgument
+{
+    public abstract uint ArgumentNum { get; }
+    public abstract ArgumentType Type { get; }
+    public object? target;
+    public abstract T TryGetTarget<T>(string arg);
+}
+
+public abstract class TBRCommand
+{
+    public virtual TBRCommandArgument[]? Arguments { get; }
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+    public abstract void Run();
+    public virtual bool CheckCommand() => true;
+}
