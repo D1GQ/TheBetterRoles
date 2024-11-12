12f1d7da22faa2840e7fbe75dc41fae3e48f9fbc
D1GQ <yourboylax@gmail.com>
2024-11-11 18:11:07 -0600

    GuestBuild

---
+++ Patches/GuestBuildPatch.cs
@@ -0,0 +1,30 @@
+﻿using AmongUs.GameOptions;
+using HarmonyLib;
+using TheBetterRoles.Helpers;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheBetterRoles.Patches;
+
+[HarmonyPatch(typeof(JoinGameButton))]
+public class GuestBuildPatch
+{
+    private static GameObject? instance;
+    [HarmonyPatch(nameof(JoinGameButton.Start))]
+    [HarmonyPostfix]
+    public static void Start_Postfix(JoinGameButton __instance)
+    {
+        if (instance != null) return;
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (Main.IsGuestBuild && sceneName == "MMOnline")
+        {
+            var JoinGameMenu = UnityEngine.Object.Instantiate(__instance.gameObject);
+            instance = JoinGameMenu;
+            __instance.transform.parent.gameObject.DestroyObj();
+        }
+    }
+}
