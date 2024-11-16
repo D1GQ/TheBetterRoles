40f9abbe23b99d1eb46f87ff0a7c7df7887ef240
D1GQ <yourboylax@gmail.com>
2024-11-15 19:44:18 -0600

    Set all visors in front of hats

---
+++ Patches/CustomHatPatches/VisorManagerPatches.cs
@@ -0,0 +1,36 @@
+﻿using Cpp2IL.Core.Extensions;
+using HarmonyLib;
+using TheBetterRoles.Managers;
+
+namespace TheBetterRoles.Patches;
+
+[HarmonyPatch(typeof(HatManager))]
+internal static class VisorManagerPatches
+{
+    private static bool isRunning;
+    private static bool isLoaded;
+    private static List<HatData> allHats;
+
+    [HarmonyPatch(nameof(HatManager.GetVisorById))]
+    [HarmonyPrefix]
+    private static void GetVisorByIdPrefix(HatManager __instance)
+    {
+        if (isRunning || isLoaded) return;
+
+        isRunning = true;
+
+        var visors = __instance.allVisors.ToList().Clone();
+        foreach (var visor in visors)
+        {
+            visor.behindHats = false;
+        }
+        isLoaded = true;
+    }
+
+    [HarmonyPatch(nameof(HatManager.GetHatById))]
+    [HarmonyPostfix]
+    private static void GetVisorByIdPostfix()
+    {
+        isRunning = false;
+    }
+}
