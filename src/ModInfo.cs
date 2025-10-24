2e55eae8b9db112457d7dbf51589debc92326b4a
D1GQ <yourboylax@gmail.com>
2025-10-23 19:36:33 -0500

    Update github

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -13,7 +13,7 @@ internal class ModInfo
     internal const string PluginGuid = "com.ten.thebetterroles";
     internal const string PluginVersion = "0.0.1";
     internal const string ReleaseDate = "10.21.2025"; // mm/dd/yyyy
-    internal const string Github = "https://github.com/D1GQ/TheBetterRoles-Public";
+    internal const string Github = "https://github.com/D1GQ/TheBetterRoles";
     internal const string Discord = "https://discord.gg/vjYrXpzNAn";
     internal static bool IsGuestBuild { get; private set; } = false && !(Main.MyData.IsSponsor() || Main.MyData.IsTester() || Main.MyData.IsDev());
 }
